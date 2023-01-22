a5f696eff625f8dd53266e1af7ac22fba61e5d1a
Dhairyash Gupta <101561153+Dhairyash-1@users.noreply.github.com>
2023-01-22 14:48:10 +0530

    card slider fully responsive done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const mobilenav = document.querySelector(".nav-mobile");
-
+const iconSwitch = document.querySelector(".iconsSwitch");
+console.log(iconSwitch);
 hamburger.addEventListener("click", () => {
   mobilenav.classList.toggle("nav-show");
+  // iconSwitch.classList.remove("uli-bars");
+  // iconSwitch.classList.add("uli-times");
 });
---
+++ js/swiper.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centerSlide: "true",
+  // slidesPerGroup: 3,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
